441d8a2bd0ac5a2627005599005229ba52192d02
diego alejandro vélez <vegodiego@hotmail.com>
2021-09-24 20:38:34 -0500

    Se configuraron CORS

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/config/WebConfig.java
@@ -0,0 +1,24 @@
+package co.com.sofka.okr.c2.api.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebFluxConfigurer corsConfigure() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
